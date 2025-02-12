43fd8acd56ea034db81892f59fa5819624a1c875
FSBalbuena <fsbalbuena@gmail.com>
2025-02-12 15:00:43 -0300

    Place the initial markup

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,28 +1,111 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import Page from "@/app/page";
+import GameOfLife from "@/app/page";
 
-describe("Page", () => {
-  it("renders a heading", () => {
-    render(<Page />);
+describe("GameOfLife", () => {
+  describe("renders", () => {
+    it('User should be able to see the title "Conway\'s game of life"', () => {
+      render(<GameOfLife />);
 
-    const heading = screen.getByRole("heading", { level: 1 });
+      const heading = screen.getByRole("heading", { level: 1 });
 
-    expect(heading).toBeInTheDocument();
-    expect(heading).toHaveTextContent("Conway's Game of Life");
+      expect(heading).toBeInTheDocument();
+      expect(heading).toHaveTextContent("Conway's Game of Life");
+    });
+
+    it('User should be able to see a description "A cellular automaton simulation."', () => {
+      render(<GameOfLife />);
+
+      const description = screen.getByRole("paragraph");
+
+      expect(description).toBeInTheDocument();
+      expect(description).toHaveTextContent("A cellular automaton simulation.");
+    });
+    it('User should be able to see the "Next state" button', () => {
+      render(<GameOfLife />);
+
+      const button = screen.getByRole("button", {
+        name: "Next state",
+      });
+
+      expect(button).toBeInTheDocument();
+      expect(button).toHaveTextContent("Next state");
+    });
+
+    it('User should be able to see the "Advance Steps"', () => {
+      render(<GameOfLife />);
+
+      const button = screen.getByRole("button", {
+        name: "Advance state",
+      });
+
+      const input = screen.getByRole("spinbutton");
+
+      expect(button).toBeInTheDocument();
+      expect(input).toBeInTheDocument();
+      expect(button).toHaveTextContent("Advance state");
+    });
+
+    it('User should be able to see the "Play Forever" checkbox', () => {
+      render(<GameOfLife />);
+
+      const checkbox = screen.getByRole("checkbox", {
+        name: "Play Forever",
+      });
+
+      expect(checkbox).toBeInTheDocument();
+    });
+
+    it('"Play Forever" checkbox should be unchecked ', () => {
+      render(<GameOfLife />);
+
+      const checkbox = screen.getByRole("checkbox", {
+        name: "Play Forever",
+      });
+
+      expect(checkbox).toBeInTheDocument();
+      expect(checkbox).not.toBeChecked();
+    });
+
+    it("renders homepage unchanged", () => {
+      //mostly for style changes
+      //"User should be able to see the grid"
+      const { container } = render(<GameOfLife />);
+      expect(container).toMatchSnapshot();
+    });
   });
 
-  it("renders a description", () => {
-    render(<Page />);
-
-    const description = screen.getByRole("paragraph");
-
-    expect(description).toBeInTheDocument();
-    expect(description).toHaveTextContent("A cellular automaton simulation.");
+  xdescribe("behaviour", () => {
+    xdescribe("Grid", () => {
+      xit("User should be able to select the initial state", () => {});
+    });
+    xdescribe("Controls", () => {
+      xdescribe("when click on Next step", () => {
+        xit('User should be able to click on "Next state" button', () => {});
+        xit("User should be able to see a loading state", () => {});
+        xit("User should be able to see the game's next state", () => {});
+      });
+      xdescribe("when checks Play Forever", () => {
+        xit('User should be able to check "Play Forever" checkbox', () => {});
+        xit("User should be able to see the game's next state every second", () => {});
+        xit('User should be able to see a message "Automated Game" on top of the grid', () => {});
+        xit("Grid should be disabled", () => {});
+        xit('User should be able to uncheck "Play Forever" checkbox', () => {});
+      });
+      xdescribe("when uses Advance Steps", () => {
+        xit('User should be able to type a number on "Advance Steps" input', () => {});
+        xit('User should be able to click on "Advance steps" button.', () => {});
+        xit("User should be able to see the game's next state", () => {});
+        xit("User should not be able to place a negative number, or zero.", () => {});
+      });
+    });
   });
 
-  it("renders homepage unchanged", () => {
-    const { container } = render(<Page />);
-    expect(container).toMatchSnapshot();
+  xdescribe("errors", () => {
+    xdescribe("User should be able to see an error message", () => {
+      xit("when he places a negative number", () => {});
+      xit("when there is an internal error", () => {});
+    });
+    xit("User should be redirected if he places a wrong url.", () => {});
   });
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
+const GRID_ID = "game-of-life-canvas";
 export default function Home() {
   return (
-    <main className="flex flex-col items-center p-10 pt-20">
+    <main className="flex flex-col align-center items-center p-10 pt-20 md:w-3/5 lg:w-1/2 xl:w-2/5 mx-auto">
       <h1 className="text-4xl mb-4">Conway&apos;s Game of Life</h1>
       <p>A cellular automaton simulation.</p>
+      <canvas id={GRID_ID} className="w-full h-full"></canvas>
+      <label htmlFor="play-forever" className="flex gap-2 p-1 self-end">
+        <input type="checkbox" id="play-forever" />
+        Play Forever
+      </label>
+      <div role="group" className="flex w-full justify-center gap-10">
+        <button
+          type="button"
+          className="fold-bold rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-yellow-400 hover:text-gray-900"
+        >
+          Next state
+        </button>
+        <div className="flex">
+          <input
+            type="number"
+            min={1}
+            className="border-2 p-1 rounded-md rounded-r-none w-10 text-center"
+          />
+          <button
+            type="button"
+            className="fold-bold rounded rounded-l-none border-2 border-black bg-black px-3 py-1 text-base font-bold text-white transition duration-100 hover:bg-gray-900 hover:text-yellow-500"
+          >
+            Advance state
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
